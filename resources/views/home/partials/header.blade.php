a38c4e8ff386ba26b9ad4b8f528dd6020ca29ce7
Asraf2asif <asraf2asif@gmail.com>
2023-01-03 22:43:44 +0600

    for mobile responsive nav: complete

--- resources/views/home/partials/header.blade.php
+++ resources/views/home/partials/header.blade.php
@@ -1,29 +1,36 @@
 <!-- Header Area Starts -->
 <header
-	class="shadow-md right-0 md:right-1/2 mr-4 w-40 md:w-4/5 py-1 px-0 bg-white absolute top-5 z-1 md:transform md:translate-x-1/2 md:translate-y-0 rounded md:mx-auto xl:w-11/12"
+	class="shadow-md left-0 lg:left-1/2 ml-4 w-56 lg:w-4/5 py-1 px-0 bg-white absolute top-5 z-1 lg:transform lg:-translate-x-1/2 lg:translate-y-0 rounded lg:mx-auto xl:w-11/12"
 >
-	<div class="mx-4 md:flex md:items-center">
-		<div class="md:px-4 md:shrink-0">
-			<a href="index.html"
-				><img src="assets/images/logo/logo.png" alt="logo" class="m-auto max-w-[200px] w-9/12 p-5 h-auto md:m-0"
-			/></a>
-		</div>
-		<div class="md:flex-1">
-			<div class="custom-navbar">
-				<span></span>
-				<span></span>
-				<span></span>
+	<div class="mx-4 lg:flex lg:items-center">
+		<div class="flex items-center">
+			<div class="lg:shrink-0">
+				<a href="index.html"
+					><img src="assets/images/logo/logo.png" alt="logo" class="m-auto max-w-[200px] min-w-[130px] w-9/12 px-2 py-4 lg:p-5 h-auto lg:m-0"
+				/></a>
 			</div>
-			<ul class="pb-2 md:pb-0 md:flex md:flex-wrap md:items-center md:justify-end">
+				<div class="nav-ham p-4 lg:hidden cursor-pointer transition ease-in-out duration-75 open">
+					@foreach ([1, 2, 3] as $data)
+		    		<span class="bg-gray-500 block w-5 h-[3px] my-1 mx-0"></span>
+		    	@endforeach
+				</div>		
+				<div class="nav-ham p-4 lg:hidden cursor-pointer relative transition ease-in-out duration-75 close hidden">
+					<span class="bg-gray-500 block w-5 h-[3px] my-1 mx-0 absolute top-3.5 right-4 transform rotate-45"></span>
+	    		<span class="bg-gray-500 block w-5 h-[3px] my-1 mx-0  absolute top-3.5 right-4 transform -rotate-45"></span>
+				</div>
+		</div>
+		
+		<div class="lg:flex-1 nav-collapse hidden lg:block">			
+			<ul class="pb-2 lg:pb-0 lg:flex lg:flex-wrap lg:items-center lg:justify-end">
 				@foreach ($navdata as $data)
-				     <li class="p-2.5 md:p-4 text-right text-sm md:font-bold text-slate-800 uppercase md:text-left">
+				     <li class="p-2.5 lg:p-4 text-right text-sm lg:font-bold text-slate-800 uppercase lg:text-left">
 				     	<a href={{ $data['href'] }}>{{ $data['text'] }}</a>
 				     </li> 
 				@endforeach
 				    
 				
 				@if (Route::has('login')) @auth
-				<div class="m-2 flex justify-end md:mx-4 md:block">
+				<div class="m-2 flex justify-end lg:mx-4 lg:block">
 					<x-jet-dropdown align="right" width="48">
 						<x-slot name="trigger">
 							@if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
@@ -94,12 +101,12 @@
 					</x-jet-dropdown>
 				</div>
 				@else
-				<li class="text-right p-4 text-sm font-bold text-slate-800 uppercase md:text-left">
+				<li class="text-right p-2.5 text-sm font-bold text-slate-800 uppercase lg:text-left">
 					<a href="{{ route('login') }}" class="text-sm text-gray-500">Login</a>
 				</li>
 
 				@if (Route::has('register'))
-				<li class="text-right p-4 text-sm font-bold text-slate-800 uppercase md:text-left">
+				<li class="text-right p-2.5 text-sm font-bold text-slate-800 uppercase lg:text-left">
 					<a href="{{ route('register') }}" class="text-sm text-gray-500"
 						>Register</a
 					>
